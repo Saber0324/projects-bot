79c568888288e651c1e0fa8799b175b3e6450ded
abusaeed-shuvo <warrior.coc1971@gmail.com>
2026-03-31 22:03:31 +0600

    Added: 1. !projects that gives current projets links 2. a fun command if you ping hux-bot with the question `is this true?`

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,3 +1,4 @@
+import random
 import discord
 from discord.ext import commands
 
@@ -14,13 +15,30 @@ class Fun(commands.Cog):
         await ctx.send("Meow too!")
 
     @commands.command()
-    async def hog(self,ctx):
+    async def hog(self, ctx):
         await ctx.send("All hail the supreme leader")
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def say(self, ctx, *, arg):
         await ctx.send(arg)
+        
+    @commands.Cog.listener()
+    async def on_message(self, message:discord.Message):
+        if message.author.bot:
+            return
+        if self.bot.user in message.mentions:
+            content = message.content.lower()
+            
+            if "is this true?" in content:
+                text_responses=["Yes","No","Maybe","Ask Hog"]
+                gif_responses=["https://tenor.com/bU4oZ.gif"]
+                
+                if random.random() < 0.5:
+                    await message.reply(random.choice(text_responses))
+                else:
+                    await message.reply(random.choice(gif_responses))
+        await self.bot.process_commands(message)
 
 async def setup(bot):
     await bot.add_cog(Fun(bot))
