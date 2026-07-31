61015f28b6e0b52d97d5cc1e0a14f327bf0aed78
Saber_0324 <250929174+Saber0324@users.noreply.github.com>
2026-07-30 23:50:49 -0400

    Change extract_code to take advantage of the new pattern

--- src/hux/templates/parse.py
+++ src/hux/templates/parse.py
@@ -3,14 +3,11 @@ import re
 CODE_PATTERN = r"```+\s*(?P<language>\S*)\n(?P<code>.*?)```+(?:\n(?P<stdin>[^\n]*))?"
 
 
-def extract_code(pattern: str, text: str) -> tuple[str, str] | tuple[str, str, str]:
+def extract_code(pattern: str, text: str) -> dict[str, str]:
     match = re.search(pattern, text, re.DOTALL | re.IGNORECASE)
     if match:
         if "`" in match.group(2):
-            return match.group(1), "tried codeblock escaping"
-        if "bf" in match.group(1) or "brainfuck" in match.group(1):
-            match_bf = re.search(BF_PATTERN, text, re.DOTALL | re.IGNORECASE)
-            if match_bf:
-                return match_bf.group(1), match_bf.group(2) + "\n" + match_bf.group(3)
-        return match.group(1), match.group(2)
-    return "Language unmatched", "No code"
+            return {"lang": match.group(1), "code": "Tried codeblock escaping"}
+        print(match.groupdict())
+        return match.groupdict()
+    return {"lang": "Language unmatched", "code": "No code"}
